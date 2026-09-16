8e30b9cbdae53feb8d8f4b7df5b31a5a9f51ccd4
Yassine-Elamri34 <elamriyassin34@gmail.com>
2026-09-15 17:21:44 -0700

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
