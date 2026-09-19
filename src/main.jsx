5f81b0e74a9208200e81298addcb2188ab5909f5
Yassine-Elamri34 <elamriyassin34@gmail.com>
2026-09-19 10:11:41 -0700

    ready for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -9,8 +9,8 @@ createRoot(
   document.getElementById("root")
 ).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
